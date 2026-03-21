72e96c9fb69ce5b96e0a56d8efa1dda0564a6b68
Etudiant MasterSID <etudiant@mastersid.fr>
2026-03-21 23:10:50 +0100

    ajout du DAO Room AppDao

--- app/src/main/java/fr/mastersid/etudiant/template/data/local/dao/AppDao.kt
+++ app/src/main/java/fr/mastersid/etudiant/template/data/local/dao/AppDao.kt
@@ -1,1 +1,18 @@
 package fr.mastersid.etudiant.template.data.local.dao
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import fr.mastersid.etudiant.template.data.local.entity.QuestionEntity
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface AppDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertAll(questions: List<QuestionEntity>)
+
+    @Query("SELECT * FROM ${QuestionEntity.TABLE_NAME} ORDER BY last_activity_date DESC")
+    fun getQuestionsFlow(): Flow<List<QuestionEntity>>
+}
