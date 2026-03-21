220c5ff7368d8b43b2926839ed98e6596068a514
Etudiant MasterSID <etudiant@mastersid.fr>
2026-03-21 23:01:01 +0100

    ajout de l'interface Retrofit StackOverflowApiService

--- app/src/main/java/fr/mastersid/etudiant/template/data/remote/api/ApiService.kt
+++ app/src/main/java/fr/mastersid/etudiant/template/data/remote/api/ApiService.kt
@@ -1,1 +1,10 @@
 package fr.mastersid.etudiant.template.data.remote.api
+
+import fr.mastersid.etudiant.template.data.remote.dto.QuestionsEnvelopeDto
+import retrofit2.http.GET
+
+interface StackOverflowApiService {
+
+    @GET("questions?pagesize=20&order=desc&sort=activity&site=stackoverflow&filter=withbody")
+    suspend fun getActiveQuestions(): QuestionsEnvelopeDto
+}
