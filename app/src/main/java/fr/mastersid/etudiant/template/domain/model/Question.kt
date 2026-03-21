ec21459cb8a6598df0e6a23c58e2f9933f59ad3e
Etudiant MasterSID <etudiant@mastersid.fr>
2026-03-21 22:43:56 +0100

    domain model Question

--- app/src/main/java/fr/mastersid/etudiant/template/domain/model/Question.kt
+++ app/src/main/java/fr/mastersid/etudiant/template/domain/model/Question.kt
@@ -1,1 +1,9 @@
 package fr.mastersid.etudiant.template.domain.model
+
+data class Question(
+    val id: Int,
+    val title: String,
+    val answerCount: Int,
+    val lastActivityDate: Long = 0L,
+    val body: String = ""
+)
