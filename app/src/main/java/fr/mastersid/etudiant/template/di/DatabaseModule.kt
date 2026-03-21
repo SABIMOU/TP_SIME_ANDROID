bf1d7eefb46c8a59fc564063133a70e57be5c972
Etudiant MasterSID <etudiant@mastersid.fr>
2026-03-21 23:37:42 +0100

    ajout du DatabaseModule (Room, DAO)

--- app/src/main/java/fr/mastersid/etudiant/template/di/DatabaseModule.kt
+++ app/src/main/java/fr/mastersid/etudiant/template/di/DatabaseModule.kt
@@ -1,1 +1,32 @@
 package fr.mastersid.etudiant.template.di
+
+import android.content.Context
+import androidx.room.Room
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import fr.mastersid.etudiant.template.data.local.dao.AppDao
+import fr.mastersid.etudiant.template.data.local.database.AppDatabase
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DatabaseModule {
+
+    @Provides
+    @Singleton
+    fun provideDatabase(
+        @ApplicationContext context: Context
+    ): AppDatabase = Room.databaseBuilder(
+        context,
+        AppDatabase::class.java,
+        AppDatabase.DATABASE_NAME
+    ).build()
+
+    @Provides
+    @Singleton
+    fun provideDao(database: AppDatabase): AppDao =
+        database.appDao()
+}
