5ec9319fbf176994c44938ed565ef6241e68ba72
Etudiant MasterSID <etudiant@mastersid.fr>
2026-03-21 23:40:37 +0100

    ajout du QuestionsUiState

--- app/src/main/java/fr/mastersid/etudiant/template/presentation/items/QuestionsUiState.kt
+++ app/src/main/java/fr/mastersid/etudiant/template/presentation/items/QuestionsUiState.kt
@@ -1,1 +1,13 @@
 package fr.mastersid.etudiant.template.presentation.items
+
+import fr.mastersid.etudiant.template.domain.model.Question
+
+data class QuestionsUiState(
+    val questions      : List<Question> = emptyList(),
+    val isUpdating     : Boolean        = false,
+    val onlyNotAnswered: Boolean        = false,
+    val errorMessage   : String?        = null
+) {
+    val displayedQuestions: List<Question>
+        get() = if (onlyNotAnswered) questions.filter { it.answerCount == 0 } else questions
+}
