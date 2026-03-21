97ddd5875631cd031f75e047f610acdbb7696b06
Etudiant MasterSID <etudiant@mastersid.fr>
2026-03-21 22:45:26 +0100

    ajout de QuestionsResponse (Idle, Pending, Error)

--- app/src/main/java/fr/mastersid/etudiant/template/domain/repository/QuestionsResponse.kt
+++ app/src/main/java/fr/mastersid/etudiant/template/domain/repository/QuestionsResponse.kt
@@ -1,1 +1,9 @@
 package fr.mastersid.etudiant.template.domain.repository
+
+import fr.mastersid.etudiant.template.domain.model.Question
+
+sealed interface QuestionsResponse {
+    data class Idle(val questions: List<Question>) : QuestionsResponse
+    data object Pending : QuestionsResponse
+    data class Error(val message: String) : QuestionsResponse
+}
