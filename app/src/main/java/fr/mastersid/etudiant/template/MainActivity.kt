561a7a0b1d21e8a68153e5d9c7e3df18e916dd23
Etudiant MasterSID <etudiant@mastersid.fr>
2026-03-21 23:45:12 +0100

    ajout de MainActivity avec QuestionsScreen

--- app/src/main/java/fr/mastersid/etudiant/template/MainActivity.kt
+++ app/src/main/java/fr/mastersid/etudiant/template/MainActivity.kt
@@ -4,13 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import dagger.hilt.android.AndroidEntryPoint
+import fr.mastersid.etudiant.template.presentation.items.QuestionsScreen
+import fr.mastersid.etudiant.template.presentation.theme.AppTheme
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            // TODO: ajouter ton Screen ici
+            AppTheme {
+                QuestionsScreen()
+            }
         }
     }
 }
