1ead2a489320145cefda3c725495c1bc89d57095
Etudiant MasterSID <etudiant@mastersid.fr>
2026-03-21 23:35:48 +0100

    ajout du NetworkModule (Moshi, OkHttp, Retrofit)

--- app/src/main/java/fr/mastersid/etudiant/template/di/NetworkModule.kt
+++ app/src/main/java/fr/mastersid/etudiant/template/di/NetworkModule.kt
@@ -1,1 +1,49 @@
 package fr.mastersid.etudiant.template.di
+
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import fr.mastersid.etudiant.template.data.remote.api.StackOverflowApiService
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    @Provides
+    @Singleton
+    fun provideMoshi(): Moshi =
+        Moshi.Builder()
+            .addLast(KotlinJsonAdapterFactory())
+            .build()
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(): OkHttpClient =
+        OkHttpClient.Builder()
+            .addInterceptor(HttpLoggingInterceptor().apply {
+                level = HttpLoggingInterceptor.Level.BODY
+            })
+            .build()
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(moshi: Moshi, client: OkHttpClient): Retrofit =
+        Retrofit.Builder()
+            .baseUrl("https://api.stackexchange.com/2.3/")
+            .client(client)
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .build()
+
+    @Provides
+    @Singleton
+    fun provideApiService(retrofit: Retrofit): StackOverflowApiService =
+        retrofit.create(StackOverflowApiService::class.java)
+}
