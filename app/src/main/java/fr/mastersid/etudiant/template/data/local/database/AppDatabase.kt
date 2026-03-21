735e153e184cb900f590e4876ace30da14c93ee9
Etudiant MasterSID <etudiant@mastersid.fr>
2026-03-21 23:14:41 +0100

    ajout de la base de données Room AppDatabase

--- app/src/main/java/fr/mastersid/etudiant/template/data/local/database/AppDatabase.kt
+++ app/src/main/java/fr/mastersid/etudiant/template/data/local/database/AppDatabase.kt
@@ -1,1 +1,20 @@
 package fr.mastersid.etudiant.template.data.local.database
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import fr.mastersid.etudiant.template.data.local.dao.AppDao
+import fr.mastersid.etudiant.template.data.local.entity.QuestionEntity
+
+@Database(
+    entities     = [QuestionEntity::class],
+    version      = 1,
+    exportSchema = false
+)
+abstract class AppDatabase : RoomDatabase() {
+
+    abstract fun appDao(): AppDao
+
+    companion object {
+        const val DATABASE_NAME = "stack_database"
+    }
+}
