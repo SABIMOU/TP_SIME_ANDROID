a6515f557e9b2f2b989a435f38976117aef0b661
Etudiant MasterSID <etudiant@mastersid.fr>
2026-03-21 23:39:20 +0100

    ajout du AppModule (liaison interface → implémentation)

--- app/src/main/java/fr/mastersid/etudiant/template/di/AppModule.kt
+++ app/src/main/java/fr/mastersid/etudiant/template/di/AppModule.kt
@@ -1,1 +1,18 @@
 package fr.mastersid.etudiant.template.di
+
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ViewModelComponent
+import fr.mastersid.etudiant.template.data.repository.RemoteQuestionsRepository
+import fr.mastersid.etudiant.template.domain.repository.QuestionsRepository
+
+@Module
+@InstallIn(ViewModelComponent::class)
+abstract class AppModule {
+
+    @Binds
+    abstract fun bindQuestionsRepository(
+        impl: RemoteQuestionsRepository
+    ): QuestionsRepository
+}
